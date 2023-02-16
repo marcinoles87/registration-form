07b8182988a7617bc3ce2114c892df6f4f945dd8
marcinoles87 <marcinoles87@gmail.com>
2023-02-16 21:36:06 +0100

    add function

--- src/components/form.js
+++ src/components/form.js
@@ -1,1 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+export default function Form() {
+    
+    let [name,setName] = useState('')
+
+const handleOnChange = (e) => {
+        console.log(e.target.value)
+        setName = (e) =>{
+            name = e.target.value
+
+        }
+        }
+    
+  return (
+
+    
+    <div>
+        <h1>Your Name</h1>
+        <input type="text" onChange={handleOnChange}></input>
+        {name}
+    </div>
+  )
+}
