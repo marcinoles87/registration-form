918ef45f0f600b789e44fabeffd86c4f7322d65c
marcinoles87 <marcinoles87@gmail.com>
2023-02-17 20:46:53 +0100

    add function

--- src/components/form.js
+++ src/components/form.js
@@ -1,19 +1,22 @@
 import React, { useState } from 'react'
+import './form.css'
 
 export default function Form() {
     
     let [name,setName] = useState('')
+    let [lastName,setLastName] = useState('')
 
 
-    const fullName = `${name}`
+    const fullName = `${name} ${lastName}` 
         
     
   return (
 
     
     <div>
-        <h1>Your Name</h1>
+        <h1>Your Name Form</h1>
         <input type="text" value={name} onChange={e => setName(e.target.value)}></input>
+        <input type="text" value={lastName} onChange={e => setLastName(e.target.value)}></input>
         {fullName}
     </div>
   )
